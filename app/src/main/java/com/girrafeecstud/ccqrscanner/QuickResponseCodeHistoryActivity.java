8a15e43e4a56747dd0987028ef201227ddaa918a
Girrafeec <ivan_zal@inbox.ru>
2021-11-23 15:49:46 +0300

    added qrcodehistory activity and recview adapter

---
+++ app/src/main/java/com/girrafeecstud/ccqrscanner/QuickResponseCodeHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.girrafeecstud.ccqrscanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuickResponseCodeHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick_responce_code_history);
+    }
+}
