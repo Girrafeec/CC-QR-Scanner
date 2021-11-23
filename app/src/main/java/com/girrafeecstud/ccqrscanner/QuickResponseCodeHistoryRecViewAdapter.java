fe7dff420688dae80aa7844c714c2d6c87d2a04c
Girrafeec <ivan_zal@inbox.ru>
2021-11-23 15:53:49 +0300

    added qrhistoryitem layout

--- app/src/main/java/com/girrafeecstud/ccqrscanner/QuickResponseCodeHistoryRecViewAdapter.java
+++ app/src/main/java/com/girrafeecstud/ccqrscanner/QuickResponseCodeHistoryRecViewAdapter.java
@@ -1,4 +1,16 @@
 package com.girrafeecstud.ccqrscanner;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class QuickResponseCodeHistoryRecViewAdapter {
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+        }
+    }
 }
